abaf5d192f1872d517272d73114e259a3971f810
aliaksandr_lautsou <aliaksandr.lautsou@lseg.com>
2023-09-27 18:09:52 +0200

    task 1: first commit

---
+++ src/test/java/pl/kempa/saska/kafkastreamstasks/KafkaStreamsTasksAppTests.java
@@ -0,0 +1,13 @@
+package pl.kempa.saska.kafkastreamstasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsTasksAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
